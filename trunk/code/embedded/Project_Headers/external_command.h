761cbdb445d157fcc0c2d4994660a92096c3a23d
thomaswhays@gmail.com <thomaswhays@gmail.com@f14fbc8f-1dcb-f0ae-3185-34a542073c79>
2013-02-01 14:43:36 +0000

    Creating enum for external commands.

---
+++ trunk/code/embedded/Project_Headers/external_command.h
@@ -0,0 +1,20 @@
+/*
+ * external_command.h
+ *
+ *  Created on: Feb 1, 2013
+ *      Author: ulab
+ */
+
+#ifndef EXTERNAL_COMMAND_H_
+#define EXTERNAL_COMMAND_H_
+
+typedef enum {
+	
+	SET_KP,
+	SET_KI,
+	SET_KD
+		
+} ExternalCommand;
+
+
+#endif /* EXTERNAL_COMMAND_H_ */
